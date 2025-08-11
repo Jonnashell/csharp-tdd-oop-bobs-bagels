b69412c0a19ac4c6a8da79e657d53e29d5bee2a6
Jonatan Berg Romundgard <jonatan.romundgard@no.experis.com>
2025-08-11 13:26:50 +0200

    Added tests for buying 6 and 12 bagels.

---
+++ exercise.tests/ExtensionTests.cs
@@ -0,0 +1,42 @@
+﻿using exercise.main;
+using exercise.main.Items;
+
+namespace exercise.tests;
+
+public class ExtensionTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void SimpleBagelDiscountTest()
+    {
+        // Arrange
+        Basket basket1 = new Basket();
+        Basket basket2 = new Basket();
+
+        basket1.ChangeCapacity(50);
+        basket2.ChangeCapacity(50);
+
+        Bagel bagel = new Bagel(sku: "BGLO", price: 0.49, name: "Bagel", variant: "Onion");
+
+        // Act
+        for (int i = 0; i <= 6; i++)
+        {
+            basket1.Add(bagel);
+        }
+
+        for (int i = 0; i <= 12; i++)
+        {
+            basket2.Add(bagel);
+        }
+
+
+        // Assert
+        //Assert.That(basket1.TotalCost == 2.49 && basket2.TotalCost == 3.99);
+        Assert.That(basket1.TotalCost, Is.EqualTo(2.49));
+        Assert.That(basket2.TotalCost, Is.EqualTo(3.99));
+    }
+}
